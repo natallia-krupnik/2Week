42a80f84dee03baf67a2c7f11a6b02fc327e3216
Natallia Krupnik <natalliakrup@gmail.com>
2023-09-18 19:12:01 +0200

    auth

--- src/middleware/authGuardMiddleware.ts
+++ src/middleware/authGuardMiddleware.ts
@@ -1,5 +1,6 @@
 import {NextFunction, Request, Response} from "express";
 import {HTTP_STATUSES} from "../types/statutes";
+import {blogsRouter} from "../routers/blogs-router";
 
 // const expectedAuthHeader = 'admin:qwerty'
 const expectedAuthHeader = 'Basic YWRtaW46cXdlcnR5'
@@ -7,20 +8,20 @@ export const authGuardMiddleware = (req: Request, res:Response, next: NextFuncti
 
     const authHeader = req.headers.authorization
 
-    if(!authHeader || !authHeader.startsWith('Basic ')) {
+    if(!authHeader || authHeader !==  expectedAuthHeader) {
         res.sendStatus(HTTP_STATUSES.unauthorized_401)
         return
     }
 
-    const splitHeader = authHeader.split(' ')[1]
-    // const enCodedHeader = atob(splitHeader)
-    const enCodedHeader = Buffer.from(splitHeader, 'base64').toString()
-
+    // const splitHeader = authHeader.split(' ')[1]
+    // // const enCodedHeader = atob(splitHeader)
+    // const enCodedHeader = Buffer.from(splitHeader, 'base64').toString()
+    // console.log(enCodedHeader)
+    // // if(enCodedHeader !== expectedAuthHeader) {
+    // //     res.sendStatus(HTTP_STATUSES.unauthorized_401)
+    // // }
     // if(enCodedHeader !== expectedAuthHeader) {
-    //     res.sendStatus(HTTP_STATUSES.unauthorized_401)
+    //    return res.sendStatus(HTTP_STATUSES.unauthorized_401)
     // }
-    if(enCodedHeader !== expectedAuthHeader) {
-        res.sendStatus(HTTP_STATUSES.unauthorized_401)
-    }
     return next()
 }
