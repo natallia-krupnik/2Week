222860dc99d0050a75475c123fa7f7e466e9607b
Natallia Krupnik <natalliakrup@gmail.com>
2023-09-18 18:37:51 +0200

    auth

--- src/middleware/authGuardMiddleware.ts
+++ src/middleware/authGuardMiddleware.ts
@@ -1,7 +1,8 @@
 import {NextFunction, Request, Response} from "express";
 import {HTTP_STATUSES} from "../types/statutes";
 
-const expectedAuthHeader = 'admin:qwerty'
+// const expectedAuthHeader = 'admin:qwerty'
+const expectedAuthHeader = 'Basic YWRtaW46cXdlcnR5'
 export const authGuardMiddleware = (req: Request, res:Response, next: NextFunction) => {
 
     const authHeader = req.headers.authorization
@@ -13,12 +14,12 @@ export const authGuardMiddleware = (req: Request, res:Response, next: NextFuncti
 
     const splitHeader = authHeader.split(' ')[1]
     // const enCodedHeader = atob(splitHeader)
-    const enCodedHeader = Buffer.from(splitHeader, 'base64')
+    const enCodedHeader = Buffer.from(splitHeader, 'base64').toString()
 
     // if(enCodedHeader !== expectedAuthHeader) {
     //     res.sendStatus(HTTP_STATUSES.unauthorized_401)
     // }
-    if(enCodedHeader.toString() !== expectedAuthHeader) {
+    if(enCodedHeader !== expectedAuthHeader) {
         res.sendStatus(HTTP_STATUSES.unauthorized_401)
     }
     return next()
